beef18b41186b0a03e9532c541e971f2755d5400
DrazZ34 <jfvillac@hotmail.com>
2023-12-07 12:18:29 +0100

    Ajout affichage index.html

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Car
 
+
 def index(request):
     cars = Car.objects.all()
     context = {'cars': cars}
