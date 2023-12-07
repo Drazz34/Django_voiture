66757bc4b8ecac9b58c501e59ea522016ded6584
DrazZ34 <jfvillac@hotmail.com>
2023-12-07 12:44:50 +0100

    ajout page détail car

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Car
+
+
+def detail(request, car_id):
+    car = get_object_or_404(Car, pk=car_id)
+    context = {'car': car}
+    return render(request, 'cars/detail.html', context)
 
 
 def index(request):
