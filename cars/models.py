b726d4e3ba418a3031c6a4652b02dfb0422f2c75
DrazZ34 <jfvillac@hotmail.com>
2023-12-07 12:47:58 +0100

    ajout champ note

--- cars/models.py
+++ cars/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Car(models.Model):
     marque = models.CharField(max_length=45)
     modele = models.CharField(max_length=45, verbose_name="Modèle")
     annee = models.IntegerField(verbose_name="Année")
     cylindree = models.IntegerField(verbose_name="Cylindrée en cm3")
     version = models.CharField(max_length=45)
+    note = models.IntegerField(max_length=2, default="Non notée")
