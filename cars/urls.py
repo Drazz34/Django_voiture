52dcf611aae76665f1e9dce551d673cdcf2d9180
DrazZ34 <jfvillac@hotmail.com>
2023-12-07 15:10:47 +0100

    Ajout formulaire forms.py sur page note.html

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:car_id>/', views.detail, name='detail'),
-
+    path('note/<int:car_id>/', views.note, name='note')
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Car
+from .forms import CarForm
 
 
 def detail(request, car_id):
@@ -9,7 +10,21 @@ def detail(request, car_id):
     return render(request, 'cars/detail.html', context)
 
 
+def note(request, car_id):
+    car = get_object_or_404(Car, pk=car_id)
+    if request.method == 'POST':
+        form = CarForm(request.POST, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect('detail', car_id=car.id)
+    else:
+        form = CarForm(instance=car)
+
+    context = {'form': form, 'car': car}
+    return render(request, 'cars/note.html', context)
+
 def index(request):
     cars = Car.objects.all()
     context = {'cars': cars}
     return render(request, 'cars/index.html', context)
+
